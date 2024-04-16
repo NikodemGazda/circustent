76442c8d8be1f2250df44c23b0408950dd65bb84
NikodemGazda <nikigazda@gmail.com>
2024-04-16 15:12:27 -0400

    bracket missing

--- device_libraries/circustent_ipu.cpp
+++ device_libraries/circustent_ipu.cpp
@@ -310,8 +310,9 @@ void tensorDecomp(long unsigned int row, long unsigned int col, long unsigned in
     v_con_randomIndices = poprand::uniform(graph, &c_con_rand_seed, 0, v_con_randomIndices, poplar::INT, 0, row*col-1, seq);
     if (MODERUN == RAND) {
         for (int i = 0; i < row*col; i++) {
-        db_name = "v_con_randomIndices[" + std::to_string(i) + "]";
-        seq.add(poplar::program::PrintTensor(db_name, v_con_randomIndices[0]));
+            db_name = "v_con_randomIndices[" + std::to_string(i) + "]";
+            seq.add(poplar::program::PrintTensor(db_name, v_con_randomIndices[i]));
+        }
     }
     // if (MODERUN == RAND) {
     //     for (int i = 0; i < row*col; i++) {
